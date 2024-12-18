49a6f075e7c7fe868504a2ec1e70b48ca5fa7983
Shreyansh284 <shreyanshranpariya@gmail.com>
2024-12-18 18:07:44 +0530

    util code added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 // require
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js"
+import { app } from './app.js';
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT|| 8000,()=>{
+        console.log(`server is running on port ${process.env.PORT|| 8000}`)
+    })
+})
+
+.catch((err)=>{
+    console.log("Connection error",err)
+})
 
 // import express from "express"
 // const app=express()
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+// higher order function
+// promisee
+const asyncHandler=(requestHandler)=>{
+    return (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).
+        catch((err)=>next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+// try catch
+// const asyncHandler=(fn)=>async(req,res,next)=>{
+//     try{
+//         await fn(req,res,next)
+//     }
+//     catch(error)
+//     {
+//         res.status(error.code||500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
